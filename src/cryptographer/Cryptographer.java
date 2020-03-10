8f18a5ea64c34898786a4c0a23c12960c9c2eccf
Mohammad Naveed <mohammadusman94@hotmail.ca>
2020-03-10 01:31:25 -0400

    Initial commit

---
+++ src/cryptographer/Cryptographer.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cryptographer;
+
+/**
+ *
+ * @author musmannaveed
+ */
+public class Cryptographer {
+
+  
+    
+    public static void main(String[] args) {
+       
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new CryptographerUI().setVisible(true);
+            }
+        });
+        }
+  
+    }
+    
+
